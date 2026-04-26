be5948ffa9b83e3c279e2110e410870f69208a60
Daphne Lin <daphnelin35@gmail.com>
2026-04-26 13:04:57 -0400

    fix CORS for Vercel frontend

--- api.py
+++ api.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://seat-sniper-ashy.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
