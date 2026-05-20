81a6501240a91bccae46cd00ea4b5bc12daf0914
Daphne Lin <daphnelin35@gmail.com>
2026-05-20 18:31:49 -0400

    fix cron

--- workers/arq_worker.py
+++ workers/arq_worker.py
@@ -54,12 +54,13 @@ async def enqueue_all_courses(ctx):
 
 class WorkerSettings:
     functions = [check_course_job]
-    cron_jobs = [...]
+    cron_jobs = [
+        cron(enqueue_all_courses, minute={0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59})
+    ]
     redis_settings = RedisSettings.from_dsn(os.getenv("REDIS_URL", "redis://localhost:6379"))
     max_jobs = 10
     job_timeout = 30
     retry_jobs = True
     max_tries = 3
-    keep_result = 60  # only keep job results for 60 seconds
+    keep_result = 60
     keep_result_forever = False
-    
